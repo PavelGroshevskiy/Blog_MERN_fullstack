eafd1f0f2908b35794983de7c502a5a7b30772d4
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2022-08-12 20:02:37 +0700

    add mongoDB, User validation

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import express from 'express'
+import mongoose from "mongoose";
+import {validationResult} from "express-validator";
+
+
+import {registerValidation} from './validation/auth.js'
+
+mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0.no1co.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('DB is OK')
+})
+    .catch((err)=>console.log('DB error', err))
 
 const app =  express()
+
+app.use(express.json( ))
 
 app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.post('/login', (req, res) => {
-    res.json({
-        "success": true
-    })
+app.post('/auth/register', registerValidation , (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors.array()) //валидация непройдена
+    }
+    res.json(
+        {'success': true}  //валидация пройдена
+    )
 })
 
 app.listen(4444, (err) => {
---
+++ validation/auth.js
@@ -0,0 +1,9 @@
+import {body} from "express-validator";
+
+export const registerValidation = [ // валидация
+    body('email', "Неверный емайл").isEmail(),
+    body('password',"Длина пароля менее 5 символов").isLength({min: 5}),
+    body('fullName',"Длина имени пользователя менее 3 символов").isLength({min: 3}),
+    body('avatarUrl', "Неверная ссылка на аватарку").optional().isURL()
+
+]
